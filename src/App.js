e227b20177bbb8e0d815581c069daa01a5ad68e9
Tobias Maier <maiertobias@outlook.com>
2020-09-23 09:38:44 +0200

    Adapt headline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello GitHub Workflow <span role="img" aria-label="Hello">🙋</span>
         </p>
         <a
           className="App-link"
