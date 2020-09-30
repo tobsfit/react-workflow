94ab583543443c9642f5d26df429a4226f01ffd2
Tobias Maier <maiertobias@outlook.com>
2020-09-30 11:21:02 +0200

    feat(core): discover core package

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -788,14 +788,26 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  throw(new Error("Some Error Message"));
+  // throw(new Error("Some Error Message"));
+  
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+
   const name = core.getInput('who-to-greet');
+  core.setSecret(name) // wird zu ***
+
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  // Expandable
+  core.startGroup('Logging github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello") // setzt eine env variable die wir in der yml datei nutzen können
 } catch(err) {
   core.setFailed(err.message) // Nur dann schlägt der Workflow fehl.
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  throw(new Error("Some Error Message"));
+  // throw(new Error("Some Error Message"));
+  
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+
   const name = core.getInput('who-to-greet');
+  core.setSecret(name) // wird zu ***
+
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  // Expandable
+  core.startGroup('Logging github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello") // setzt eine env variable die wir in der yml datei nutzen können
 } catch(err) {
   core.setFailed(err.message) // Nur dann schlägt der Workflow fehl.
 }
