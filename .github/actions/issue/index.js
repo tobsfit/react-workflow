dce737c3ba81add6f05af21a0a3d9975dd7335fa
Tobias Maier <maiertobias@outlook.com>
2020-09-30 11:55:02 +0200

    feat(action): add own action for issue creation

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,26 @@
+// .github/actions/issue/index.js
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
+
+    const octokit = new github.GitHub(token); // Statt octokit könnten wir auch fetch nutzen
+    const response = await octokit.issues.addAssignees({
+      ...github.context.repo,
+      title,
+      body,
+      // braucht ein Array
+      assignees: assignees ? assignees.split('\n'): undefined
+    });
+
+    core.setOutput('issue', JSON.stringify(response.data)) // Output muss immer ein String sein
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+run();
