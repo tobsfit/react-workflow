26ca87eb40137843f11170189fa48ff81bc779ce
Tobias Maier <maiertobias@outlook.com>
2020-10-02 10:02:44 +0200

    feat(action): add action for sending slack message via php

---
+++ .github/actions/slack/script.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once 'vendor/autoload.php';
+Requests::register_autoloader();
+
+// So bekommen wir die Inputs aus unserem yml file
+// var_dump($argv);
+// var_dump($_ENV);
+
+echo "::debug ::Sending a request to slack\n";
+
+$response = Requests::post(
+  $_ENV['INPUT_SLACK_WEBHOOK'],
+  // array of headers
+  array(
+    "Content-type" => "application/json"
+  ),
+  // data
+  json_encode(array(
+    'blocks' =>
+    array(
+      0 =>
+      array(
+        'type' => 'section',
+        'text' =>
+        array(
+          'type' => 'mrkdwn',
+          'text' => $_ENV['INPUT_MESSAGE'],
+        ),
+      ),
+      1 =>
+      array(
+        'type' => 'section',
+        'fields' =>
+        array(
+          0 =>
+          array(
+            'type' => 'mrkdwn',
+            'text' => '*Repository:*
+            {$_ENV["GITHUB_REPOSITORY"]}',
+          ),
+          1 =>
+          array(
+            'type' => 'mrkdwn',
+            'text' => '*Event:*
+            {$_ENV["GITHUB_EVENT_NAME"]}',
+          ),
+          2 =>
+          array(
+            'type' => 'mrkdwn',
+            'text' => '*Ref:*
+            {$_ENV["GITHUB_REF"]}',
+          ),
+          3 =>
+          array(
+            'type' => 'mrkdwn',
+            'text' => '*SHA:*
+            {$_ENV["GITHUB_SHA"]}',
+          ),
+        ),
+      ),
+    ),
+  )),
+);
+
+echo "::group::Slack Response\n";
+var_dump($response);
+echo "::endgroup::\n";
+
+if (!$response->success) {
+  echo $response->body;
+  exit(1);
+}
